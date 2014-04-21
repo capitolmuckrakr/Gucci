60b9819581f50dbaf98bb3b02fb0fbb78ebcc8d8
acohen <alex@capitolmuckraker.com>
2014-04-20 21:15:32 -0400

    connecting filing and search to gucci

--- lib/gucci.rb
+++ lib/gucci.rb
@@ -1,5 +1,12 @@
 require "gucci/version"
+require "gucci/filing"
+require "gucci/search"
 
 module Gucci
-  # Your code goes here...
+  def self.search(opts={})
+    Search.new(opts={})
+  end
+  def self.filing(filing_id,opts={})
+    Filing.new(filing_id,opts={})
+  end
 end
