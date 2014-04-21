58272c8f29d502b2d0e1d6406e38dd979e7c3fa2
acohen <alex@capitolmuckraker.com>
2014-04-20 21:04:24 -0400

    adding filing and search

---
+++ lib/gucci/filing.rb
@@ -0,0 +1,260 @@
+require 'nokogiri'
+require 'open-uri'
+require 'ensure/encoding'
+
+module Gucci
+
+    class Filing
+
+      attr_accessor :xml, :download_dir
+
+      attr_reader :filing_id, :summary,  :issues, :updates, :parsingproblems
+
+      def initialize(filing_id,opts={})
+        @filing_id = filing_id
+        @download_dir = opts[:download_dir]
+        @parsingproblems = []
+        @summary = {}
+        @issues = []
+        @updates = {}
+      end
+
+      def download
+        File.open(file_path, 'w') do |file|
+          begin
+            file << open(filing_url).read
+          rescue
+            file << open(filing_url).read.ensure_encoding('UTF-8', :external_encoding => Encoding::UTF_8,:invalid_characters => :drop)
+          end
+        end
+        self
+      end
+
+      def parse
+        begin
+          @xml = Nokogiri::XML(File.open(file_path,"r"))
+          return @xml
+        rescue Exception => e
+          puts e.message
+          puts e.backtrace.inspect
+        end
+      end
+
+#grab our single fields(organizationName, reportYear, income, expenses, etc), remove carriage returns, assign keys
+     def summary
+       summary_hash = Mapper.new
+       begin
+         parse.children.each do |node| #only one child we need
+           if node.element? #should pass unless filing is malformed
+             node.children.each do |childnode| # access top-level fields
+               if childnode.children.count < 2 && childnode.node_name != 'text' #test for single-value fields such as registrantname, clientname, etc. Skip linefeeds.
+                 begin
+                   summary_hash[childnode.name.to_sym] = nil
+                   unless childnode.content.strip.empty?
+                     begin
+                       summary_hash[childnode.name.to_sym] = childnode.content #transform into hash
+                     rescue Exception=>e
+                       parse_problem(e,'@summary.' + childnode.name.to_s)
+                     end
+                   end
+                 rescue Exception=>e
+                   parse_problem(e,childnode)
+                 end
+               end
+             end
+           end
+         end
+       rescue Exception=>e
+         parse_problem(e,'@summary')
+       end
+       data ||= summary_hash
+       data
+     end
+
+     class Mapper < Hash
+       def method_missing(name)
+         return self[name] if key? name
+         super.method_missing name
+       end
+       def respond_to_missing?(name, include_private = false)
+         key? name || super
+       end
+     end
+#grab our fields for alis(issues,agencies,lobbyists,etc), remove blank text nodes, assign keys
+     def issues
+       begin
+         multi = multinodes[0].dup
+         multi.children.each do |m|
+           if m.node_name != 'text'
+             issuefields = Mapper.new
+             m.children.map{ |i| issuefields[i.name.to_sym] = nil if i.children.count < 2 }
+             m.children.map do |i|
+               unless i.content.strip.empty?
+                 issuefields[i.name.to_sym] = i.content if i.children.count < 2
+               end
+             end
+             m.children.map{ |i| issuefields[i.name.to_sym] = i unless i.children.count < 2 }
+             @lobbyists = []
+             issuefields[:lobbyists].children.each do |l|
+               next if l.content == 'N'
+               @lobbyist = Mapper.new
+               l.children.each do |f|
+                 @lobbyist[f.name.to_sym] = nil
+                 @lobbyist[f.name.to_sym] = f.content unless f.content.strip.empty?
+               end
+               @lobbyists.push(@lobbyist) unless @lobbyist.values.join.strip == 'N'
+             end
+             issuefields[:lobbyists] = @lobbyists #need to assign one lobbyist hash for empties
+             @agencies = issuefields[:federal_agencies].split(",").each {|agency| agency.strip! if agency.respond_to? :strip! } if issuefields[:federal_agencies].respond_to? :split
+             issuefields[:federal_agencies] = @agencies
+             @descriptions = []
+             if issuefields[:specific_issues].kind_of? Nokogiri::XML::Element
+               issuefields[:specific_issues].children.each do |si|
+                 @descriptions.push(si.content)
+               end
+               issuefields[:specific_issues] = @descriptions
+             end
+             @issues.push(issuefields)
+           end
+         end
+         @issues
+       rescue Exception=>e
+         parse_problem(e,'@issues')
+       end
+     end
+#grab our fields for updates(change of address,inactive lobbyists,inactive issues,etc), remove blank text nodes, assign keys
+     def updates
+       @updates = Mapper.new
+       multi = multinodes[1].dup
+       multi.children.each do |m|
+         if m.node_name != 'text'
+           if m.children.count < 2
+               @updates[m.name.to_sym] = nil
+               @updates[m.name.to_sym] = m.content unless m.content.strip.empty? || m.children.children.count > 1
+           end
+           @updates[m.name.to_sym] = m if m.children.count > 1 || m.children.count < 2 && m.children.children.count > 1
+           @inactive_lobbyists = []
+           begin
+             @updates[:inactive_lobbyists].children.each do |l|
+                 next if l.content.empty?
+               @inactive_lobbyist = Mapper.new
+               l.children.each do |f|
+                 @inactive_lobbyist[f.name.to_sym] = f.content
+               end
+               @inactive_lobbyists.push(@inactive_lobbyist)
+             end
+             @updates[:inactive_lobbyists] = @inactive_lobbyists
+           rescue Exception=>e
+             parse_problem(e,'@updates[:inactive_lobbyists]')
+           end
+           @inactive_alis = []
+           begin
+             @updates[:inactive_ALIs].children.each do |ia|
+               next if ia.content.empty?
+               @inactive_ali = ia.content
+               @inactive_alis.push(@inactive_ali)
+             end
+             @updates[:inactive_ALIs] = @inactive_alis
+           rescue Exception=>e
+             parse_problem(e,'@updates[:inactive_ALIs]')
+           end
+           @inactiveOrgs = []
+           begin
+             @updates[:inactiveOrgs].children.each do |ia|
+               next if ia.content.empty?
+               @inactiveOrg = ia.content
+               @inactiveOrgs.push(@inactive_ali)
+             end
+             @updates[:inactiveOrgs] = @inactiveOrgs
+           rescue Exception=>e
+             parse_problem(e,'@updates[:inactiveOrgs]')
+           end
+           @inactive_ForeignEntities = []
+           begin
+             @updates[:inactive_ForeignEntities].children.each do |ia|
+               next if ia.content.empty?
+               @inactive_ForeignEntity = ia.content
+               @inactive_ForeignEntities.push(@inactive_ForeignEntity)
+             end
+             @updates[:inactive_ForeignEntities] = @inactive_ForeignEntities
+           rescue Exception=>e
+             parse_problem(e,'@updates[:inactive_ForeignEntities]')
+           end
+           @affiliatedOrgs = []
+           begin
+             @updates[:affiliatedOrgs].children.each do |a|
+               next if a.content.empty?
+               @affiliatedOrg = Mapper.new
+               a.children.each do |f|
+                 @affiliatedOrg[f.name.to_sym] = f.content
+               end
+               @affiliatedOrgs.push(@affiliatedOrg)
+             end
+             @updates[:affiliatedOrgs] = @affiliatedOrgs
+           rescue Exception=>e
+             parse_problem(e,'@updates[:affiliatedOrgs]')
+           end
+           @foreignEntities = []
+           begin
+             @updates[:foreignEntities].children.each do |a|
+               next if a.content.empty?
+               @foreignEntity = Mapper.new
+               a.children.each do |f|
+                 @foreignEntity[f.name.to_sym] = f.content
+               end
+               @foreignEntities.push(@foreignEntity)
+             end
+             @updates[:foreignEntities] = @foreignEntities
+           rescue Exception=>e
+             parse_problem(e,'@updates[:foreignEntities]')
+           end
+         end
+       end
+       @updates
+     end
+
+     def parse_problem(e,problemfield)
+       problem = {}
+       problem["field"] = problemfield
+       problem["message"] = e.message.to_s
+       problem["backtrace"] = e.backtrace.inspect.to_s
+       parsingproblems.push(problem) unless e.message.to_s == 'undefined method `children\' for nil:NilClass'
+     end
+
+     def multinodes
+       multinodelist = []
+       parse.children.each do |node| #only one child
+         if node.element? #should pass unless filing is malformed or xslt
+           node.children.each do |childnode| # access top-level fields
+             if childnode.children.count > 1 && childnode.node_name != 'text' #skip single-value top level fields such as registrantname, clientname, etc. Skip linefeeds.
+               childnode.children.each do |m|
+                 m.children.map{ |i| i.remove if i.name == 'text' && i.blank? }
+                 m.children.map{ |i| i.children.map{ |i2| i2.remove if i2.name == 'text' && i2.blank? } }
+                 m.children.map{ |i| i.children.map{ |i2| i2.children.map{ |i3| i3.remove if i3.name == 'text' && i3.blank? } } }
+               end
+               multinodelist.push(childnode)
+             end
+           end
+         end
+       end
+       multinodelist
+     end
+
+     def filing_url_base
+       'http://disclosures.house.gov/ld/pdfform.aspx?id='
+     end
+
+     def filing_url
+       filing_url_base + filing_id
+     end
+
+     def file_path
+       File.join(download_dir.to_s, file_name.to_s)
+     end
+
+     def file_name
+        "#{filing_id}.xml"
+     end
+
+    end
+end
---
+++ lib/gucci/search.rb
@@ -0,0 +1,473 @@
+require 'watir-webdriver'
+require 'headless'
+require 'tmpdir'
+require 'date'
+
+module Gucci
+
+    class Search
+
+      attr_accessor :download_dir, :search_params
+
+      def initialize(opts={})
+        @download_dir = opts.delete(:download_dir) || Dir.tmpdir
+        File.delete("#{@download_dir}/Disclosures.CSV") if File.exists?("#{@download_dir}/Disclosures.CSV")
+        @browser = browser
+        @search_params = validate_params(make_params(opts))
+        search(@search_params)
+      end
+
+      def browser
+        #headless = Headless.new
+        #headless.start
+        profile = Selenium::WebDriver::Firefox::Profile.new
+        profile['browser.download.folderList'] = 2
+        profile['browser.download.dir'] = @download_dir
+        profile["browser.helperApps.neverAsk.saveToDisk"] = "text/csv, application/octet-stream"
+        driver = Selenium::WebDriver.for :firefox, :profile => profile
+        browser = Watir::Browser.new(driver)
+        browser.goto 'disclosures.house.gov/ld/ldsearch.aspx'
+        return browser
+      end
+
+      def search(params)
+        param_count = 0
+        selected_params = {}
+        params.each_key do |param|
+          param_count += 1
+          selected_params[param_count] = param
+          param_id = "DropDownList#{param_count}"
+          @browser.select_list(:id => "#{param_id}").select "#{param}"
+          sleep 1
+        end
+        @browser.button(:name => 'cmdSearch').click
+        selected_params.keys.sort.each do |param_order|
+          param_id = Lobby::VALID_PARAMS.keys.include?(selected_params[param_order]) ? "DropDownList#{param_order}0" : "TextBox#{param_order}"
+          if Lobby::VALID_PARAMS.keys.include?(selected_params[param_order])
+            @browser.select_list(:id => "#{param_id}").select "#{params[selected_params[param_order]]}"
+          else
+            @browser.text_field(:id => "#{param_id}").set "#{params[selected_params[param_order]]}"
+          end
+          sleep 1
+        end
+        @browser.button(:name => 'cmdSearch').click
+        @browser.radio(:id => 'RadioButtonList1_1' ).set # for CSV download
+        @browser.button(:name => 'cmdDownload').click #download a file of the search results, extension is CSV, but it's actually tab separated
+      end
+
+      def results()
+        filings = []
+        open("#{@download_dir}/Disclosures.CSV","r").each_line{|l| l.gsub!('"',''); filings << l.split("\t")[0..-2]}
+        filings.shift
+        filings.sort_by!{|e| e[0].to_i}.reverse! #largest filing_id is newest?
+        return filings
+      end
+
+      def parse_results(&block)
+        keys = [:filing_id, :registrant_id, :registrant_name, :client_name, :filing_year, :filing_period, :lobbyists]
+        parsed_results = []
+        results.each do |row|
+          row_hash = Hash[*keys.zip(row).flatten]
+          search_result = SearchResult.new(row_hash)
+          if block_given?
+            yield search_result
+          else
+            parsed_results << search_result
+          end
+        end
+        block_given? ? nil : parsed_results
+      end
+
+      def make_params(search_params)
+        {
+        'Registrant Name' => search_params[:registrant_name] || '', #validate?
+        'Client Name' => search_params[:client_name] || '', #validate?
+        'House ID' => search_params[:house_id] || '', #validate?
+        'Filing Period' => search_params[:filing_period] || '',
+        'Filing Type' => search_params[:filing_type] || '',
+        'Filing Year' => search_params[:filing_year] || '',
+        'Issue Code' => search_params[:issue_code] || '',
+        'Lobbyist Name' => search_params[:lobbyist_name] || '', #validate?
+        'Affiliated Country' => search_params[:affiliated_country] || '',
+        'Affiliated Name' => search_params[:affiliated_name] || '', #validate?
+        'Amount Reported' => search_params[:amount_reported] || '', #validate?
+        'Client Country' => search_params[:client_country] || '',
+        'Client PPB Country' => search_params[:client_ppb_country] || '',
+        'Client State' => search_params[:client_state] || '',
+        'Foreign Entiry PPB Country' => search_params[:foreign_entity_ppb_country] || '', #typo in field name on House form
+        'Foreign Entity Country' => search_params[:foreign_entity_country] || '',
+        'Foreign Entity Name' => search_params[:foreign_entity_name] || '', #validate?
+        'Foreign Entity Ownership' => search_params[:foreign_entity_ownership] || '', #validate?
+        'Government Entity' => search_params[:government_entity] || '', #validate?
+        'Issue Data' => search_params[:issue_data] || '', #validate?
+        'Lobbyist Covered' => search_params[:lobbyist_covered] || '',
+        'Lobbyist Covered Position' => search_params[:lobbyist_position] || '', #validate?
+        'Lobbyists Full Name Inactive' => search_params[:lobbyist_inactive] || '', #validate?
+        'Registrant Country' => search_params[:registrant_country] || '',
+        'Registrant PPB Country' => search_params[:registrant_ppb_country] || ''
+        }
+      end
+
+      def validate_params(params)
+        raise ArgumentError, "At least one search parameter must be given, possible parameters are #{Lobby::VALID_PARAMS.keys.join(', ')}" if params.values.all? { |x| x.to_s.empty? }
+        params.delete_if { |k,v| v.to_s.empty? }
+        raise ArgumentError, "No more than six search parameters are permitted" if params.keys.count > 6
+        invalid_params = []
+        Lobby::VALID_PARAMS.each_pair do |k,v|
+          if params.keys.include?(k)
+            invalid_params.push("#{params[k]} is invalid for #{k}, permitted values are #{v.join(', ')}\n") unless Lobby::VALID_PARAMS[k].include?( params[k] )
+          end
+        end
+        raise ArgumentError, "#{invalid_params.count} error(s)\n#{invalid_params.join.chomp}" unless invalid_params.empty?
+        params
+      end
+
+    end
+
+    class SearchResult
+      attr_reader :filing_id, :registrant_id, :registrant_name, :client_name, :filing_year, :filing_period, :lobbyists
+      def initialize(row_hash)
+        @filing_id = row_hash[:filing_id]
+        @registrant_id = row_hash[:registrant_id]
+        @registrant_name = row_hash[:registrant_name]
+        @client_name = row_hash[:client_name]
+        @filing_year = row_hash[:filing_year]
+        @filing_period = row_hash[:filing_period]
+        @lobbyists = row_hash[:lobbyists].to_s.split("|").map{|l|l.to_s.strip} || nil
+      end
+    end
+
+      COUNTRIES = {
+                "AFG" => "AFGHANISTAN",
+  		"ALB" => "ALBANIA",
+  		"ALG" => "ALGERIA",
+  		"ASA" => "AMERICAN SAMOA",
+  		"AND" => "ANDORRA",
+  		"ANG" => "ANGOLA",
+  		"ANT" => "ANTIGUA/BARBUDA",
+  		"ARG" => "ARGENTINA",
+  		"ARM" => "ARMENIA",
+  		"ARU" => "ARUBA",
+  		"AUS" => "AUSTRALIA",
+  		"AUT" => "AUSTRIA",
+  		"AZE" => "AZERBAIJAN",
+  		"BAH" => "BAHAMAS",
+  		"BRN" => "BAHRAIN",
+  		"BAN" => "BANGLADESH",
+  		"BAR" => "BARBADOS",
+  		"BLR" => "BELARUS",
+  		"BEL" => "BELGIUM",
+  		"BIZ" => "BELIZE",
+  		"BEN" => "BENIN",
+  		"BER" => "BERMUDA",
+  		"BHU" => "BHUTAN",
+  		"BOL" => "BOLIVIA",
+  		"BIH" => "BOSNIA/HERZEGOVINA",
+  		"BOT" => "BOTSWANA",
+  		"BRA" => "BRAZIL",
+  		"IVB" => "BRITISH VIRGIN ISLANDS",
+  		"BRU" => "BRUNEI",
+  		"BUL" => "BULGARIA",
+  		"BUR" => "BURKINA FASO",
+  		"BDI" => "BURUNDI",
+		"CAM" => "CAMBODIA",
+		"CMR" => "CAMEROON",
+		"CAN" => "CANADA",
+		"CPV" => "CAPE VERDE",
+		"CAY" => "CAYMAN ISLANDS",
+		"CAF" => "CENTRAL AFRICAN REPUBLIC",
+		"CHA" => "CHAD",
+		"CHI" => "CHILE",
+		"CHN" => "CHINA",
+		"COL" => "COLOMBIA",
+		"COM" => "COMOROS",
+		"COD" => "CONGO, DEMOCRATIC REPBLIC OF THE",
+		"CGO" => "CONGO, REPUBLIC OF THE",
+		"COK" => "COOK ISLANDS",
+		"CRC" => "COSTA RICA",
+		"CIV" => "COTE D'IVOIRE",
+		"CRO" => "CROATIA (HRVATSKA)",
+		"CUB" => "CUBA",
+		"CYP" => "CYPRUS",
+		"CZE" => "CZECH REPUBLIC",
+		"DEN" => "DENMARK",
+		"DJI" => "DJIBOUTI",
+		"DMA" => "DOMINICA",
+		"DOM" => "DOMINICAN REPUBLIC",
+		"ECU" => "ECUADOR",
+		"EGY" => "EGYPT",
+		"ESA" => "EL SALVADOR",
+		"GEQ" => "EQUATORIAL GUINEA",
+		"ERI" => "ERITREA",
+		"EST" => "ESTONIA",
+		"ETH" => "ETHIOPIA",
+		"FIJ" => "FIJI",
+		"FIN" => "FINLAND",
+		"FRA" => "FRANCE",
+		"GAB" => "GABON",
+		"GAM" => "GAMBIA",
+		"GEO" => "GEORGIA",
+		"GER" => "GERMANY",
+		"GHA" => "GHANA",
+		"GRE" => "GREECE",
+		"GRN" => "GRENADA",
+		"GUM" => "GUAM",
+		"GUA" => "GUATEMALA",
+		"GUI" => "GUINEA",
+		"GBS" => "GUINEA-BISSAU",
+		"GUY" => "GUYANA",
+		"HAI" => "HAITI",
+		"HON" => "HONDURAS",
+		"HKG" => "HONG KONG",
+		"HUN" => "HUNGARY",
+		"ISL" => "ICELAND",
+		"IND" => "INDIA",
+		"INA" => "INDONESIA",
+		"IRI" => "IRAN",
+		"IRQ" => "IRAQ",
+		"IRL" => "IRELAND",
+		"ISR" => "ISRAEL",
+		"ITA" => "ITALY",
+		"JAM" => "JAMAICA",
+		"JPN" => "JAPAN",
+		"JOR" => "JORDAN",
+		"KAZ" => "KAZAKHSTAN",
+		"KEN" => "KENYA",
+		"PRK" => "KOREA, REPUBLIC OF",
+		"KOR" => "KOREA, REPUBLIC OF",
+		"KUW" => "KUWAIT",
+		"KGZ" => "KYRGYSTAN",
+		"LAO" => "LAOS, PEOPLES DEMOCRACTIC REPUBLIC",
+		"LAT" => "LATVIA",
+		"LIB" => "LEBANON",
+		"LES" => "LESOTHO",
+		"LBR" => "LIBERIA",
+		"LBA" => "LIBYAN ARAB JAMAHIRIYA",
+		"LIE" => "LIECHTENSTEIN",
+		"LTU" => "LITHUANIA",
+		"LUX" => "LUXEMBORG",
+		"MKD" => "MACEDONIA",
+		"MAD" => "MADAGASCAR",
+		"MAW" => "MALAWI",
+		"MAS" => "MALAYSIA",
+		"MDV" => "MALDIVES",
+		"MLI" => "MALI",
+		"MLT" => "MALTA",
+		"MTN" => "MAURITANIA",
+		"MRI" => "MAURITIUS",
+		"MEX" => "MEXICO",
+		"FSM" => "MICRONESIA, FEDERATED STATES OF",
+		"MDA" => "MOLDOVA, REPUBLIC OF",
+		"MON" => "MONACO",
+		"MGL" => "MONGOLIA",
+		"MAR" => "MOROCCO",
+		"MOZ" => "MOZAMBIQUE",
+		"MYA" => "MYANMAR",
+		"NAM" => "NAMIBIA",
+		"NRU" => "NAURU",
+		"NEP" => "NEPAL",
+		"NED" => "NETHERLANDS",
+		"AHO" => "NETHERLANDS ANTILLES",
+		"NZL" => "NEW ZEALAND",
+		"NCA" => "NICARAGUA",
+		"NIG" => "NIGER",
+		"NGR" => "NIGERIA",
+		"NOR" => "NORWAY",
+		"OMA" => "OMAN",
+		"PAK" => "PAKISTAN",
+		"PLW" => "PALAU",
+		"PLE" => "PALESTINE",
+		"PAN" => "PANAMA",
+		"PNG" => "PAPUA NEW GUINEA",
+		"PAR" => "PARAGUAY",
+		"PER" => "PERU",
+		"PHI" => "PHILIPPINES",
+		"POL" => "POLAND",
+		"POR" => "PORTUGAL",
+		"PUR" => "PUERTO RICO",
+		"QAT" => "QATAR",
+		"ROM" => "ROMANIA",
+		"RUS" => "RUSSIAN FEDERATION",
+		"RWA" => "RWANDA",
+		"SKN" => "SAINT KITTS &amp; NEVIS",
+		"LCA" => "SAINT LUCIA",
+		"VIN" => "SAINT VINCENT &amp; GRENADINES",
+		"SAM" => "SAMOA",
+		"SMR" => "SAN MARINO",
+		"STP" => "SAO TOME &amp; PRINCIPE",
+		"KSA" => "SAUDI ARABIA",
+		"SEN" => "SENEGAL",
+		"SEY" => "SEYCHELLES",
+		"SLE" => "SIERRA LEONE",
+		"SIN" => "SINGAPORE",
+		"SVK" => "SLOVAKIA (SLOVAK REPUBLIC)",
+		"SLO" => "SLOVENIA",
+		"SOL" => "SOLOMON ISLANDS",
+		"SOM" => "SOMALIA",
+		"RSA" => "SOUTH AFRICA",
+		"ESP" => "SPAIN",
+		"SRI" => "SRI LANKA",
+		"SUD" => "SUDAN",
+		"SUR" => "SURINAME",
+		"SWZ" => "SWAZILAND",
+		"SWE" => "SWEDEN",
+		"SUI" => "SWITZERLAND",
+		"SYR" => "SYRIAN ARAB REPUBLIC",
+		"TRE" => "TAIWAN",
+		"TJK" => "TAJIKISTAN",
+		"TAN" => "TANZANIA, UNITED REPUBLIC OF",
+		"THA" => "THAILAND",
+		"TOG" => "TOGO",
+		"TGA" => "TONGA",
+		"TRI" => "TRINIDAD &amp; TOBAGO",
+		"TUN" => "TUNISIA",
+		"TUR" => "TURKEY",
+		"TKM" => "TURKMENISTAN",
+		"UGA" => "UGANDA",
+		"UKR" => "UKRAINE",
+		"UAE" => "UNITED ARAB EMIRATES",
+		"GBR" => "UNITED KINGDOM",
+		"URU" => "URUGUAY",
+		"USA" => "USA",
+		"UZB" => "UZBEKISTAN",
+		"VAN" => "VANUATU",
+		"VEN" => "VENEZUELA",
+		"VIE" => "VIETNAM",
+		"ISV" => "VIRGIN ISLANDS",
+		"YEM" => "YEMEN",
+		"YUG" => "YUGOSLAVIA",
+		"ZAM" => "ZAMBIA",
+		"ZIM" => "ZIMBABWE"
+      }
+
+      FILING_TYPES = {
+        "MM" => "Mid-Year Report",
+        "MA" => "Mid-Year Amendment Report",
+        "MT" => "Mid-Year Termination Report",
+        "M@" => "Mid-Year Termination Amendment Report",
+        "YY" => "Year-End Report",
+        "YA" => "Year-End Amendment Report",
+        "YT" => "Year-End Termination Report",
+        "Y@" => "Year-End Termination Amendment Report",
+        "RR" => "Registration",
+        "RA" => "Registration Amendment",
+        "Q1" => "1st Quarter Report",
+        "1A" => "1st Quarter Amendment Report",
+        "1T" => "1st Quarter Termination Report",
+        "1@" => "1st Quarter Termination Amendment Report",
+        "Q2" => "2nd Quarter Report",
+        "2A" => "2nd Quarter Amendment Report",
+        "2T" => "2nd Quarter Termination Report",
+        "2@" => "2nd Quarter Termination Amendment Report",
+        "Q3" => "3rd Quarter Report",
+        "3A" => "3rd Quarter Amendment Report",
+        "3T" => "3rd Quarter Termination Report",
+        "3@" => "3rd Quarter Termination Amendment Report",
+        "Q4" => "4th Quarter Report",
+        "4A" => "4th Quarter Amendment Report",
+        "4T" => "4th Quarter Termination Report",
+        "4@" => "4th Quarter Termination Amendment Report",
+        "NR" => "New Registrant Using Web Form"
+      }
+
+      ISSUES = {
+                        "ACC" => "ACCOUNTING",
+  			"ADV" => "ADVERTISING",
+  			"AER" => "AEROSPACE",
+  			"AGR" => "AGRICULTURE",
+  			"ALC" => "ALCOHOL AND DRUG ABUSE",
+  			"ANI" => "ANIMALS",
+  			"APP" => "APPAREL/CLOTHING INDUSTRY/TEXTILES",
+  			"ART" => "ARTS/ENTERTAINMENT",
+  			"AUT" => "AUTOMOTIVE INDUSTRY",
+  			"AVI" => "AVIATION/AIRCRAFT/AIRLINES",
+  			"BAN" => "BANKING",
+			"BNK" => "BANKRUPTCY",
+			"BEV" => "BEVERAGE INDUSTRY",
+			"BUD" => "BUDGET/APPROPRIATIONS",
+			"CHM" => "CHEMICALS/CHEMICAL INDUSTRY",
+			"CIV" => "CIVIL RIGHTS/CIVIL LIBERTIES",
+			"CAW" => "CLEAN AIR AND WATER (QUALITY)",
+			"CDT" => "COMMODITIES (BIG TICKET)",
+			"COM" => "COMMUNICATIONS/BROADCASTING/RADIO/TV",
+			"CPI" => "COMPUTER INDUSTRY",
+			"CON" => "CONSTITUTION",
+			"CSP" => "CONSUMER ISSUES/SAFETY/PRODUCTS",
+			"CPT" => "COPYRIGHT/PATENT/TRADEMARK",
+			"DEF" => "DEFENSE",
+			"DIS" => "DISASTER PLANNING/EMERGENCIES",
+			"DOC" => "DISTRICT OF COLUMBIA",
+			"ECN" => "ECONOMICS/ECONOMIC DEVELOPMENT",
+			"EDU" => "EDUCATION",
+			"ENG" => "ENERGY/NUCLEAR",
+			"ENV" => "ENVIRONMENT/SUPERFUND",
+			"FAM" => "FAMILY ISSUES/ABORTION/ADOPTION",
+			"FIN" => "FINANCIAL INSTITUTIONS/INVESTMENTS/SEC",
+			"FIR" => "FIREARMS/GUNS/AMMUNITION",
+			"FOO" => "FOOD INDUSTRY (SAFETY, LABELING, ETC.)",
+			"FOR" => "FOREIGN RELATIONS",
+			"FUE" => "FUEL/GAS/OIL",
+			"GAM" => "GAMING/GAMBLING/CASINO",
+			"GOV" => "GOVERNMENT ISSUES",
+			"HCR" => "HEALTH ISSUES",
+			"HOM" => "HOMELAND SECURITY",
+			"HOU" => "HOUSING",
+			"IMM" => "IMMIGRATION",
+			"IND" => "INDIAN/NATIVE/AMERICAN AFFAIRS",
+			"INS" => "INSURANCE",
+			"INT" => "INTELLIGENCE AND SURVEILLANCE",
+			"LBR" => "LABOR ISSUES/ANTITRUST/WORKPLACE",
+			"LAW" => "LAW ENFORCEMENT/CRIME/CRIMINAL JUSTICE",
+			"MAN" => "MANUFACTURING",
+			"MAR" => "MARINE/MARITIME/BOATING/FISHERIES",
+			"MIA" => "MEDIA (INFORMATION/PUBLISHING)",
+			"MED" => "MEDICAL/DISEASE RESEARCH/CLINICAL LABS",
+			"MMM" => "MEDICARE/MEDICAID",
+			"MON" => "MINTING/MONEY/GOLD STANDARD",
+			"NAT" => "NATURAL RESOURCES",
+			"PHA" => "PHARMACY",
+			"POS" => "POSTAL",
+			"RRR" => "RAILROADS",
+			"RES" => "REAL ESTATE/LAND USE/CONSERVATION",
+			"REL" => "RELIGION",
+			"RET" => "RETIREMENT",
+			"ROD" => "ROADS/HIGHWAY",
+			"SCI" => "SCIENCE/TECHNOLOGY",
+			"SMB" => "SMALL BUSINESS",
+			"SPO" => "SPORTS/ATHLETICS",
+			"TAR" => "TARIFF (MISCELLANEOUS TARIFF BILLS)",
+			"TAX" => "TAXATION/INTERNAL REVENUE CODE",
+			"TEC" => "TELECOMMUNICATIONS",
+			"TOB" => "TOBACCO",
+			"TOR" => "TORTS",
+			"TRD" => "TRADE (DOMESTIC/FOREIGN)",
+			"TRA" => "TRANSPORTATION",
+			"TOU" => "TRAVEL/TOURISM",
+			"TRU" => "TRUCKING/SHIPPING",
+			"UNM" => "UNEMPLOYMENT",
+			"URB" => "URBAN DEVELOPMENT/MUNICIPALITIES",
+			"UTI" => "UTILITIES",
+			"VET" => "VETERANS",
+			"WAS" => "WASTE (HAZARD/SOLID/INTERSTATE/NUCLEAR)",
+			"WEL" => "WELFARE"
+      }
+
+      STATES = ["AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"]
+      VALID_PARAMS = {
+        'Filing Period' => ["Mid-Year", "Year-End", "1st Quarter", "2nd Quarter", "3rd Quarter", "4th Quarter"],
+        'Filing Type' => FILING_TYPES.values,
+        'Filing Year' => (2009..Date.today.year).map{ |y| y },
+        'Issue Code' => ISSUES.values,
+        'Affiliated Country' => COUNTRIES.values,
+        'Client Country' => COUNTRIES.values,
+        'Client PPB Country' => COUNTRIES.values,
+        'Client State' => STATES,
+        'Foreign Entiry PPB Country' => COUNTRIES.values, #typo in field name on House form
+        'Foreign Entity Country' => COUNTRIES.values,
+        'Lobbyist Covered' => ["True","False"],
+        'Registrant Country' => COUNTRIES.values,
+        'Registrant PPB Country' => COUNTRIES.values
+      }
+
+
+end
