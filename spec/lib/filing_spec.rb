36754f8be39d6ac0c76e7f2f786e8898c3d8c5d3
Alexander Cohen <alex@capitolmuckraker.com>
2014-04-23 14:00:07 -0400

    tests for lobbying report filing

--- spec/lib/filing_spec.rb
+++ spec/lib/filing_spec.rb
@@ -11,18 +11,83 @@ describe Gucci do
     #@contribution.stubs(:file_path).returns(File.join(File.dirname(__FILE__), 'data', '700704476.xml'))
   end
 
-  it "should return a filing object" do
+  it "should return a report filing object" do
     @filing = Gucci::House::Filing.new(300630565,:download_dir=>File.join(File.dirname(__FILE__), 'data'))
     @filing.should_not be_nil
   end
 
-  describe "#summary" do
+  describe "#report_summary" do
 
-    it "should return the mapped summary row" do
+    it "should return the report mapped summary row" do
       sum = @report.summary
-      #sum.should be_a_kind_of(Hash)
-      #sum[:clientName].should == "Environmental Defense Action Fund"
+      sum.should be_a_kind_of(Hash)
+      sum.clientName.should == "Environmental Defense Action Fund"
     end
   end
+
+  describe "#report_issues" do
+
+    it "should return an array" do
+      iss = @report.issues
+      iss.should be_a_kind_of(Array)
+    end
+  end
+
+  describe "#report_issue" do
+
+    it "should return a mapped issue row" do
+      iss1 = @report.issues.first
+      iss1.should be_a_kind_of(Hash)
+      iss1.issueAreaCode.should == "CAW"
+    end
+  end
+  
+  describe "#report_lobbyists" do
+
+    it "should return an array" do
+      lob = @report.issues.first.lobbyists
+      lob.should be_a_kind_of(Array)
+    end
+  end
+  
+  describe "#report_lobbyist" do
+
+    it "should return a mapped lobbyist row" do
+      lob = @report.issues.first.lobbyists
+      lob.sort_by!{|v| v.lobbyistLastName}
+      lob1 = lob.first
+      lob1.should be_a_kind_of(Hash)
+      lob1.lobbyistLastName.should == "Andress"
+    end
+  end
+
+  describe "#report_updates" do
+
+    it "should return the report mapped update row" do
+      ups = @report.updates
+      ups.should be_a_kind_of(Hash)
+      ups.clientAddress.should == nil
+    end
+  end
+  
+  describe "#report_inactive_lobbyists" do
+
+    it "should return an array" do
+      inact_lobs = @report.updates.inactive_lobbyists
+      inact_lobs.should be_a_kind_of(Array)
+      inact_lobs.count.should == 2
+    end
+  end
+  
+  describe "#report_inactive_lobbyist" do
+
+    it "should return a mapped inactive lobbyist row" do
+      inact_lobs = @report.updates.inactive_lobbyists
+      inact_lobs.sort_by!{|v| v.lastName}
+      inact_lob1 = inact_lobs.first
+      inact_lob1.should be_a_kind_of(Hash)
+      inact_lob1.lastName.should == "Carey"
+    end
+  end  
 
 end
