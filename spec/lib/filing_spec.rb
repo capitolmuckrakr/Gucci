1fe805840e111d632544536d622b97aa4afb64dc
Alexander Cohen <alex@capitolmuckraker.com>
2014-04-23 14:02:39 -0400

    lobbying registration stub

--- spec/lib/filing_spec.rb
+++ spec/lib/filing_spec.rb
@@ -4,7 +4,7 @@ describe Gucci do
 
   before do
     @registration = Gucci::House::Filing.new(300611018,:download_dir=>File.join(File.dirname(__FILE__), 'data'))
-    #@registration.stubs(:file_path).returns(File.join(File.dirname(__FILE__), 'data', '300611018.xml'))
+    @registration.stubs(:file_path).returns(File.join(File.dirname(__FILE__), 'data', '300611018.xml'))
     @report = Gucci::House::Filing.new(300630565,:download_dir=>File.join(File.dirname(__FILE__), 'data'))
     @report.stubs(:file_path).returns(File.join(File.dirname(__FILE__), 'data', '300630565.xml'))
     #@contribution = Gucci::House::Filing.new(700704476,:download_dir=>File.join(File.dirname(__FILE__), 'data')
