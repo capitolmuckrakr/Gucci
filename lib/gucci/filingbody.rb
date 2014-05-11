25636bb81c5c9e7ae586235c6e1626862a3aea2e
acohen <alex@capitolmuckraker.com>
2014-05-11 02:07:12 -0400

    initial commit for filingbody

---
+++ lib/gucci/filingbody.rb
@@ -0,0 +1,23 @@
+module FilingBody
+  def parsefields(n)
+    @n = n
+    begin
+      data = []
+      multi(@n).children.each do |m|
+        if m.name != 'text'
+          parsedfields = Gucci::Mapper.new
+          m.children.map do |m1|
+            parsedfields[m1.name.to_sym] = nil
+            unless m1.content.strip.empty?
+              parsedfields[m1.name.to_sym] = m1.content
+            end
+          end
+          data.push(parsedfields)
+        end
+      end
+      data || nil
+    rescue Exception=>e
+      parse_problem(e,'parsedfields')
+    end
+  end
+end
