14b3c6687ede3a469eeb2b5c7bd5c94165c0256b
Alexander Cohen <alex@capitolmuckraker.com>
2014-05-13 14:00:59 -0400

    new test in results context; commented out results context

--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -8,26 +8,18 @@ describe Gucci do
       expect { Gucci::House::Search.new }.to raise_error(ArgumentError)
     end
 
-    #it "should raise ArgumentError if :form_type is given without another parameter" do
-    #  expect { Fech::Search.new(:form_type => "F3") }.to raise_error(ArgumentError)
-    #end
-
-    #it "should not raise an error if :committee_id is given by itself" do
-    #  expect { Fech::Search.new(:committee_id => "C00431171") }.to_not raise_error(ArgumentError)
-    #end
-
-    #it "should not raise an error if :form_type is given with another parameter" do
-    #  expect { Fech::Search.new(:form_type => "F3", :date => Date.new(2013, 5, 29)) }.to_not raise_error(ArgumentError)
-    #end
-
   end
 
   context "results" do
 
-    it "should return an array of results" do
-      search = Gucci::House::Search.new(:filing_year => Date.today.year, :issue_code => "DEFENSE")
-      search.results.should be_a_kind_of(Array)
-    end
+    #it "should return an array of results" do
+    #  search = Gucci::House::Search.new(:filing_year => Date.today.year, :issue_code => "DEFENSE")
+    #  search.results.should be_a_kind_of(Array)
+    #end
+    
+    #it "should raise ArgumentError if more than 2000 search results are returned" do
+    #  expect { Gucci::House::Search.new(:filing_year => 2013) }.to raise_error(ArgumentError)
+    #end
     
   end
 
