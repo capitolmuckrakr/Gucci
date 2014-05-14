1fec41e9eb06dcd21320aca22ef6e50d9324ffc6
acohen <alex@capitolmuckraker.com>
2014-05-13 22:57:11 -0400

    rename descriptions

--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Gucci do
 
-  context "creating" do
+  describe "creating search" do
 
     it "should raise ArgumentError if no parameters are given" do
       expect { Gucci::House::Search.new }.to raise_error(ArgumentError)
@@ -10,17 +10,17 @@ describe Gucci do
 
   end
 
-  context "results" do
+  describe "search results" do
 
     #it "should return an array of results" do
     #  search = Gucci::House::Search.new(:filing_year => Date.today.year, :issue_code => "DEFENSE")
     #  search.results.should be_a_kind_of(Array)
     #end
-    
+
     #it "should raise ArgumentError if more than 2000 search results are returned" do
     #  expect { Gucci::House::Search.new(:filing_year => 2013) }.to raise_error(ArgumentError)
     #end
-    
+
   end
 
 end
