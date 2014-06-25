e3aae7dac489d89beb4120b99b5c8fa71c64f108
acohen <alex@capitolmuckraker.com>
2014-06-25 00:26:24 -0400

    method to turn date/time strings into time objects

--- lib/gucci/mapper.rb
+++ lib/gucci/mapper.rb
@@ -1,3 +1,20 @@
+String.class_eval do
+  def to_strtime
+    begin
+      c = self.gsub(/(\/| )/,':').split(':')
+      c[3] = c[3].to_i + 12 if c[-1] == "PM" && c.count >3
+      c.pop if c.count > 3
+      if c.count > 3
+        Time.new(c[2],c[0],c[1],c[3],c[4],c[5],"-04:00")
+      else
+        Time.new(c[2],c[0],c[1],0,0,0,"+04:00") if c.count < 4
+      end
+    rescue
+      self
+    end
+  end
+end
+
 module Gucci
   class Mapper < Hash
     def method_missing(name)
