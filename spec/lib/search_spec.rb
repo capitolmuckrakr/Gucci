785d8337872be17849238bb779878399ad4d229d
Alexander Cohen <alex@capitolmuckraker.com>
2014-09-30 20:46:47 -0400

    enabled array test using expect syntax

--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -12,10 +12,10 @@ describe Gucci do
 
   describe "search results" do
 
-    #it "should return an array of results" do
-    #  search = Gucci::House::Search.new(:filing_year => Date.today.year, :issue_code => "DEFENSE")
-    #  search.results.should be_a_kind_of(Array)
-    #end
+    it "should return an array of results" do
+      search = Gucci::House::Search.new(:filing_year => Date.today.year, :issue_code => "DEFENSE")
+      expect(search.results).to be_a_kind_of(Array)
+    end
 
     #it "should raise ArgumentError if more than 2000 search results are returned" do
     #  expect { Gucci::House::Search.new(:filing_year => 2013) }.to raise_error(ArgumentError)
